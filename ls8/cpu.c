2e946d32e9b20ad79340de6a223411e4cbdf6844
Nate Boyette <nateboyette@icloud.com>
2019-06-10 15:25:03 -0700

    Current progress. Implementing cpu_run function.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,4 +1,7 @@
 #include "cpu.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #define DATA_LEN 6
 
@@ -8,18 +11,19 @@
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,13 +35,36 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
+}
+
+int cpu_ram_read(struct cpu *cpu, int index)
+{
+  if (index > 256 || index < 0)
+  {
+    printf("Index out of range. \n");
+    return (0);
+  }
+
+  printf("%d\n", cpu->ram[index]);
+  return cpu->ram[index];
+}
+
+void cpu_ram_write(struct cpu *cpu, int element, int index)
+{
+  if (index > 256 || index < 0)
+  {
+    printf("Index out of range. \n");
+  }
+
+  cpu->ram[index] = element;
 }
 
 /**
@@ -47,9 +74,13 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
+  cpu_ram_read(cpu, 0);
+
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+
     // 2. Figure out how many operands this next instruction requires
     // 3. Get the appropriate value(s) of the operands following this instruction
     // 4. switch() over it to decide on a course of action.
@@ -63,5 +94,15 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
+  cpu = malloc(sizeof(cpu));
   // TODO: Initialize the PC and other special registers
+  cpu->pc = 0;
+  cpu->registers = malloc(sizeof(unsigned char) * 8);
+
+  // set each index in registers to 0
+  memset(cpu->registers, 0, 8 * sizeof(unsigned char));
+
+  cpu->ram = malloc(sizeof(unsigned char) * 256);
+  // set each position in ram to 0
+  memset(cpu->ram, 0, 256 * sizeof(unsigned char));
 }
