806d1d42cd3f36dc60c6436773391d199ffa3fa3
Nate Boyette <nateboyette@icloud.com>
2019-06-11 13:51:13 -0700

    Refactored cpu_load function.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -8,25 +8,39 @@
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-      // From print8.ls8
-      0b10000010, // LDI R0,8
-      0b00000000,
-      0b00001000,
-      0b01000111, // PRN R0
-      0b00000000,
-      0b00000001 // HLT
-  };
 
+  FILE *fp;
+  fp = fopen(filename, "r");
+  char line[1024];
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++)
+  if (fp == NULL)
   {
-    cpu->ram[address++] = data[i];
+    fprintf(stderr, "file not found\n");
+    exit(1);
   }
 
+  // printf("Something is working\n");
+
+  while (fgets(line, sizeof(line), fp) != NULL)
+  {
+
+    char *endptr;
+
+    unsigned char v = strtoul(line, &endptr, 2);
+
+    if (endptr == line)
+    {
+      continue;
+    }
+
+    cpu->ram[address] = v;
+    address++;
+  }
+
+  fclose(fp);
   // TODO: Replace this with something less hard-coded
 }
 
@@ -81,7 +95,7 @@ void cpu_run(struct cpu *cpu)
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     ir = cpu->ram[cpu->pc];
-    // ir = cpu->ram[5];
+
     unsigned char operandA;
     unsigned char operandB;
 
@@ -114,6 +128,7 @@ void cpu_run(struct cpu *cpu)
       break;
     default:
       printf("Unknown instructrion %02 at address %02\n", ir, cpu->pc);
+      exit(1);
     }
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "cpu.h"
+#include <stdlib.h>
 
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: comp filename\n");
+    exit(1);
+  }
+
+  
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(&cpu, argv[1]);
   cpu_run(&cpu);
 
   return 0;
